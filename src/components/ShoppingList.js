d5fb46e140c148b8ba0be67dd6ee77e2568743b0
Stella Mazzillo <stellagress5@gmail.com>
2023-05-25 16:09:01 -0400

    complete react hooks information flow lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import Filter from "./Filter";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
@@ -17,12 +18,7 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+        <Filter onCategoryChange={handleCategoryChange}/>
       </div>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
@@ -34,3 +30,57 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import Item from "./Item";
+
+// function ShoppingList({ items }) {
+//   const [selectedCategory, setSelectedCategory] = useState("All");
+
+//   function handleCategoryChange(event) {
+//     setSelectedCategory(event.target.value);
+//   }
+
+//   const itemsToDisplay = items.filter((item) => {
+//     if (selectedCategory === "All") return true;
+
+//     return item.category === selectedCategory;
+//   });
+
+//   return (
+//     <div className="ShoppingList">
+//       <div className="Filter">
+//         <select name="filter" onChange={handleCategoryChange}>
+//           <option value="All">Filter by category</option>
+//           <option value="Produce">Produce</option>
+//           <option value="Dairy">Dairy</option>
+//           <option value="Dessert">Dessert</option>
+//         </select>
+//       </div>
+//       <ul className="Items">
+//         {itemsToDisplay.map((item) => (
+//           <Item key={item.id} name={item.name} category={item.category} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ShoppingList;
